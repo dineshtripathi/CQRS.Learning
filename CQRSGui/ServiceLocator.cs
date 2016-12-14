6ebb9214aa312dabf8e5ec59d452285021af00ca
dtripathi <dtripathi@OE-VACR0GM983G6>
2016-12-14 12:25:08 +0000

    Add project files.

---
+++ CQRSGui/ServiceLocator.cs
@@ -0,0 +1,15 @@
+﻿using SimpleCQRS;
+
+namespace CQRSGui
+{
+    public static class ServiceLocator
+    {
+        public static FakeBus Bus { get; set; }
+       
+    }
+
+   public static class Derived
+    {
+        public static string c;
+    }
+}
